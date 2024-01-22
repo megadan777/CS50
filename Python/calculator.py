3cff8ea156fd5e7042134023517ba2452500d5b5
Daniel Mowbray <daniel.mowbray86@gmail.com>
2024-01-23 08:41:14 +1100

    2301
    py further functions

--- Python/calculator.py
+++ Python/calculator.py
@@ -1,6 +1,6 @@
 x = float(input("What's X? "))
 y = float(input("What's Y? "))
 
-z = round(x + y)
+z = round(x / y, 2)
 
-print(f"{z:,}")
+print(z)
---
+++ Python/hellotwo.py
@@ -0,0 +1,13 @@
+def main():
+    name = input("What's Your Name? ")
+    hello(name)
+
+
+
+
+
+
+def hello(to="world"):
+    print("hello,", to)
+
+main()
