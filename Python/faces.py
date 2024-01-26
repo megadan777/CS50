887c8ee6f1d570d041483e60d96cbf166589ca5d
Daniel Mowbray <daniel.mowbray86@gmail.com>
2024-01-26 21:39:52 +1100

    2601

--- Python/faces.py
+++ Python/faces.py
@@ -1,6 +1,7 @@
 # Creating an input from the user
 def(main):
-message = input("Enter some text: ")
+message = inputstr(("Enter some text: "))
+
 
 # replacing each space with ... (i.e., three periods).
 message = message.replace(' ', '...')
