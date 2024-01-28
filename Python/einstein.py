7c857a35880b79f885cccea79a8d4dc96e9801b0
Daniel Mowbray <daniel.mowbray86@gmail.com>
2024-01-28 20:31:18 +1100

    2801
    einstein py

---
+++ Python/einstein.py
@@ -0,0 +1,8 @@
+# Creating an input from the user
+integer = input("Enter mass to output joules: ")
+
+
+# convert emoticons to emoji automatically
+message = message.replace(":)", "🙂").replace(":(","🙁")
+
+print(message)
