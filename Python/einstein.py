dd36b3b758536cf88531372ae9f3d0129f4ec9ba
Daniel Mowbray <daniel.mowbray86@gmail.com>
2024-01-29 20:21:47 +1100

    2901
    py einstein correct code

--- Python/einstein.py
+++ Python/einstein.py
@@ -1,11 +1,11 @@
 #asking for the mass in grams
 mass = float(input("Enter the mass of object(in grams): "))
 
-#Speed of light is known and given in the question
-c = 3 * 10 ** 8
+#Create float for mass
+mass = float(mass)
 
-#calculating the energy, mass is divided by 1000 to convert it into kilogram
-Energy = (mass/1000) * c ** 2 
+#calculating the energy
+energy = int(mass * 300000000 ** 2)
 
-#printing the output
-print(round(Energy, None))
+# Printing the output
+print(f"Energy: {energy}")
