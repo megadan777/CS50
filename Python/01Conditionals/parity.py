4e531a705d41f71cb02a215ba3f51c4dc4c00bf6
Daniel Mowbray <daniel.mowbray86@gmail.com>
2024-02-15 19:45:58 +1100

    1502
    py bool

--- Python/01Conditionals/parity.py
+++ Python/01Conditionals/parity.py
@@ -1,7 +1,16 @@
-x = int(input("What's X? "))
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
 
-if x % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
-    
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+main()
