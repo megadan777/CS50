57978a6df36e63f2e7bec4a555580d45001020eb
Daniel Mowbray <daniel.mowbray86@gmail.com>
2024-02-20 07:58:16 +1100

    2002
    interpreter fixed coding

--- Python/01Conditionals/projects/interpreter.py
+++ Python/01Conditionals/projects/interpreter.py
@@ -1,2 +1,20 @@
-interpret = int(input("Enter your arithmetic expression: ")).strip()
+def main():
+    interpret = input("Enter your arithmetic expression: ")
+    convert(interpret)
 
+def convert(s):
+    x, y, z = s.split(" ")
+
+    x = float(x)
+    z = float(z)
+
+    if y == "+":
+        print(x + z)
+    elif y == "-":
+        print(x - z)
+    elif y == "*":
+        print(x * z)
+    elif y == "/":
+        print(x / z)
+
+main()
