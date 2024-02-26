b9409ff5a0b77a1a4fbd2534e02d69998382ab6f
Daniel Mowbray <daniel.mowbray86@gmail.com>
2024-02-27 06:59:39 +1100

    2702
    meal py time ranges

--- Python/01Conditionals/projects/meal.py
+++ Python/01Conditionals/projects/meal.py
@@ -1,10 +1,19 @@
 def main():
-    meal = int(input("What time is it? "))
-
+    time = input("What time is it? ").strip()
+    converted_time = convert(time)
+    if 7 <= converted_time <= 8:
+        print("breakfast time")
+    elif 12 <= converted_time <= 13:
+        print("lunch time")
+    elif 18 <= converted_time <= 19:
+        print("dinner time")
+    else:
+        print("")
 
 def convert(time):
-    ...
-
+    x, y = map(int, time.split(":"))
+    converted_time = x + y/60
+    return converted_time
 
 if __name__ == "__main__":
     main()
