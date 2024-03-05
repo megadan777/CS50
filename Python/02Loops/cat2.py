13798751e999c1bb4e534b93d690136694aa5370
Daniel Mowbray <daniel.mowbray86@gmail.com>
2024-03-05 19:21:12 +1100

    0503
    using main loop py

--- Python/02Loops/cat2.py
+++ Python/02Loops/cat2.py
@@ -1,2 +1,17 @@
-for _ in range(3):
-    print("meow")
+def main():
+    meow(get_number())
+
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 1:
+            return n
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()
