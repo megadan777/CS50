db9bd60adae73f8362cc2ab870276d4361225bf2
Daniel Mowbray <daniel.mowbray86@gmail.com>
2024-03-09 20:10:24 +1100

    0903
    py dict improve

--- Python/02Loops/hogwarts.py
+++ Python/02Loops/hogwarts.py
@@ -4,7 +4,5 @@ students = {
     "Ron": "Gryffindor",
     "Draco": "Slytherin",
 }
-print(students["Hermoine"])
-print(students["Harry"])
-print(students["Ron"])
-print(students["Draco"])
+for student in students:
+    print(student, students[student], sep=", ")
