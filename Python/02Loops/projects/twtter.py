22a21c2ca8840b68a1c0a4bf4904b067bf40e2b2
Daniel Mowbray <135515270+megadan777@users.noreply.github.com>
2024-03-18 21:03:50 +1100

    1803
    twttr py, no vowels

---
+++ Python/02Loops/projects/twtter.py
@@ -0,0 +1,10 @@
+#Get user input
+input = input("Input: ")
+#List all the vowels, these will need to be removed
+vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+
+
+
+
+for c in s:
+    print(c, end="")
