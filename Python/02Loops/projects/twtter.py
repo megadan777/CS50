f0edfd3e223e619710be08e0d3cf97b11a7759fd
Daniel Mowbray <135515270+megadan777@users.noreply.github.com>
2024-03-18 21:18:23 +1100

    1803
    working ver

--- Python/02Loops/projects/twtter.py
+++ Python/02Loops/projects/twtter.py
@@ -1,10 +1,14 @@
 #Get user input
-input = input("Input: ")
+user_input = input("Input: ")
 #List all the vowels, these will need to be removed
 vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
 
 
 
-#Replace vowels when printed
-for vowels in input:
-    print(input.replace('vowels', ''))
+# Replace vowels when printed
+modified_input = ""
+for char in user_input:
+    if char not in vowels:
+        modified_input += char
+
+print(modified_input)
